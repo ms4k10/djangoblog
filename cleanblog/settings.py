ca22dbde4fa852cc93cf973951fd5965c871a583
ms4k10 <ms4k10@gmail.com>
2019-05-01 20:29:24 +0900

    setting.py typemiss2

--- cleanblog/settings.py
+++ cleanblog/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'cleanblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-import dj_databese_url
+import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(),
